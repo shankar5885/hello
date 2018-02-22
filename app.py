2f18eb17694df3bc9c4d178c436af80dc4646100
shankar5885 <rao.shankararao@gmail.com>
2018-02-22 21:30:45 +0530

    test commit

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: <unicode-escape> -*-
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+import os, sys
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def main():
+	return ("Welcome!")
+if __name__ == "__main__":
+	app.run()
