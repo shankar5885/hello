00f9eafb69d83d89d741f781b9902cd369ef2d9d
shankar5885 <rao.shankararao@gmail.com>
2018-02-23 13:53:47 +0530

    test commit

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-	return ("********Welcome******")
+	return ("!!!!!!!!!!!!!!!!Welcome!!!!!!!!!!!!!!")
 if __name__ == "__main__":
 	app.run()
